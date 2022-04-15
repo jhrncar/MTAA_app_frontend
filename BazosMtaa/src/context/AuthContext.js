0b297dcdd89dce8d5bb238e634c0903e3b064dde
borisce <borcer99@gmail.com>
2022-04-15 23:51:37 +0200

    registration POST

--- BazosMtaa/src/context/AuthContext.js
+++ BazosMtaa/src/context/AuthContext.js
@@ -1,5 +1,6 @@
 import EncryptedStorage from 'react-native-encrypted-storage';
 import React, {createContext, useEffect, useState} from 'react';
+import Register from '../screens/Register'
 
 export const AuthContext = createContext();
 
@@ -7,9 +8,47 @@ export const AuthProvider = ({children}) => {
   const [isLogged, setLogged] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [splashLoading, setSplashLoading] = useState(false);
-  const register = (name, email, password) => {
-    setIsLoading(true); //TODOcele dorobit
+  
+  const register = (firstname, lastname,name, email, password,city,street,zipcode,phone,district) => {
+    setIsLoading(true);
+    fetch('http://192.168.100.203:8000/register/', {
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        first_name: firstname,
+        last_name: lastname,
+        email: email,
+        user_name: name, 
+        password: password,
+        city: city,
+        street: street,
+        zip_code: zipcode,
+        phone: phone,
+        district:district,
+      }),
+    })
+    .then(value => {
+      //tu budu nejake IFy na response
+      setLogged(true);
+      EncryptedStorage.setItem(
+        'loggedIn',
+       // JSON.stringify({
+         // loggedIn: true,
+       // }),
+      );
+      setIsLoading(false);
+    })
+    .catch(error => {
+      console.log(error.response);
+    });
+      
+
+    console.log('login');
   };
+
 
   const login = (username, password) => {
     setIsLoading(true);
@@ -20,8 +59,8 @@ export const AuthProvider = ({children}) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        user_name: 'filter_test', //tu bude premenna username
-        password: 'test123', //tu bude premenna password
+        user_name: username, //tu bude premenna username
+        password: password, //tu bude premenna password
       }),
     })
       .then(value => {
@@ -68,7 +107,7 @@ export const AuthProvider = ({children}) => {
     try {
       setSplashLoading(true);
       let userInfo = await EncryptedStorage.getItem('loggedIn');
-      userInfo = JSON.parse(userInfo).loggedIn;
+      //userInfo = JSON.parse(userInfo).loggedIn;
 
       if (userInfo) {
         console.log(userInfo);
--- BazosMtaa/src/screens/Register.js
+++ BazosMtaa/src/screens/Register.js
@@ -10,16 +10,33 @@ import {
 import {SafeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components';
 import {BottomNavigation, Text, useTheme, Switch} from 'react-native-paper';
-import {View, ScrollView} from 'react-native';
+import {View, ScrollView, StyleSheet} from 'react-native';
 import {Link, useNavigation} from '@react-navigation/native';
+import {Picker} from '@react-native-picker/picker'; 
 import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 import {AuthContext} from '../context/AuthContext';
+import {useState} from "react";
+import { useValidation } from 'react-simple-form-validator';
 
 const Register = ({navigation}) => {
   const {colors} = useTheme(); //Bud nebude nepovinne pole a bude to vyzerat pekne, alebo tam bude, ale bude to vyzerat zle
   const [isRequired, setIsRequired] = React.useState(true);
   const handleRequired = () => setIsRequired(!isRequired);
   const {register} = React.useContext(AuthContext);
+  
+  
+  const [email, setEmail] = useState(null);
+  const [firstname, setFirstName] = useState(null);
+  const [lastname, setLastName] = useState(null);
+  const [name, setName] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmpassword, setConfirmPassword] = useState(null);
+  const [street, setStreet] = useState(null);
+  const [zipcode, setZipcode] = useState(null);
+  const [city, setCity] = useState(null);
+  const [district, setDistrict] = useState(null)
+  const [phone, setPhone] = useState(null)
+    
   return (
     <ScrollView
       contentContainerStyle={{
@@ -42,26 +59,52 @@ const Register = ({navigation}) => {
       {isRequired && (
         <View>
           <InputStyled
+            label={'Meno'}
+            mode="outlined"
+            outlineColor={colors.tertiary}
+            onChangeText={(val) => setFirstName(val)}
+            value={firstname}
+          />
+          <InputStyled
+            label={'Priezvisko'}
+            mode="outlined"
+            outlineColor={colors.tertiary}
+            onChangeText={(val) => setLastName(val)}
+            value={lastname}
+          />
+          <InputStyled
             label={'Používateľské meno'}
             mode="outlined"
             outlineColor={colors.tertiary}
+            onChangeText={(val) => setName(val)}
+            value={name}
           />
           <InputStyled
             label={'E-mail'}
             mode="outlined"
             outlineColor={colors.tertiary}
             keyboardType="email-address"
+            onChangeText={(val) => setEmail(val)}
+            value={email}
           />
           <InputStyled
+            secureTextEntry={true}
             label={'Heslo'}
             mode="outlined"
             outlineColor={colors.tertiary}
+            onChangeText={(val) => setPassword(val)}
+            value={password}
           />
           <InputStyled
+            secureTextEntry={true}
             label={'Zopakuj heslo'}
             mode="outlined"
             outlineColor={colors.tertiary}
+            onChangeText={(val) => setConfirmPassword(val)}
+            value={confirmpassword}
           />
+
+          <Text>{name} {email} {password} {confirmpassword}</Text>
         </View>
       )}
       {!isRequired && (
@@ -70,6 +113,8 @@ const Register = ({navigation}) => {
             label={'Ulica a číslo domu'}
             mode="outlined"
             outlineColor={colors.tertiary}
+            value={street}
+            onChangeText={(val) => setStreet(val)}
           />
 
           <InputStyled
@@ -77,22 +122,117 @@ const Register = ({navigation}) => {
             mode="outlined"
             outlineColor={colors.tertiary}
             keyboardType="numeric"
+            value={zipcode}
+            onChangeText={(val) => setZipcode(val)}
           />
           <InputStyled
             label={'Mesto'}
             mode="outlined"
             outlineColor={colors.tertiary}
+            value={city}
+            onChangeText={(val) => setCity(val)}
           />
           <InputStyled
-            label={'Okres'}
+            label={'Telefón'}
             mode="outlined"
+            keyboardType="numeric"
             outlineColor={colors.tertiary}
+            value={phone}
+            onChangeText={(val) => setPhone(val)}
           />
+          <View style={styles.pickerview}>
+            <Picker
+              dropdownIconColor= 'black'
+              style={styles.picker}
+              selectedValue={district}
+              onValueChange={(itemValue, itemIndex) => setDistrict(itemValue)}
+            >
+              <Picker.Item label="Bánovce nad Bebravou" value="Bánovce nad Bebravou " />
+              <Picker.Item label="Banská Bystrica" value="Banská Bystrica" />
+              <Picker.Item label="Banská Štiavnica" value="Banská Štiavnica" />
+              <Picker.Item label="Bardejov" value="Bardejov" />
+              <Picker.Item label="Bratislava I" value="Bratislava I" />
+              <Picker.Item label="Bratislava II" value="Bratislava II" />
+              <Picker.Item label="Bratislava III" value="Bratislava III" />
+              <Picker.Item label="Bratislava IV" value="Bratislava IV" />
+              <Picker.Item label="Bratislava V" value="Bratislava V" />
+              <Picker.Item label="Brezno" value="Brezno" />
+              <Picker.Item label="Bytča" value="Bytča" />
+              <Picker.Item label="Čadca" value="Čadca" />
+              <Picker.Item label="Detva" value="Detva" />
+              <Picker.Item label="Dolný Kubín" value="Dolný Kubín" />
+              <Picker.Item label="Dunajská Streda" value="Dunajská Streda" />
+              <Picker.Item label="Galanta" value="Galanta" />
+              <Picker.Item label="Gelnica" value="Gelnica" />
+              <Picker.Item label="Hlohovec" value="Hlohovec" />
+              <Picker.Item label="Humenné" value="Humenné" />
+              <Picker.Item label="Ilava" value="Ilava" />
+              <Picker.Item label="Kežmarok" value="Kežmarok" />
+              <Picker.Item label="Komárno" value="Komárno" />
+              <Picker.Item label="Košice I" value="Košice I" />
+              <Picker.Item label="Košice II" value="Košice II" />
+              <Picker.Item label="Košice III" value="Košice III" />
+              <Picker.Item label="Košice IV" value="Košice IV" />
+              <Picker.Item label="Košice-okolie" value="Košice-okolie" />
+              <Picker.Item label="Krupina" value="Krupina" />
+              <Picker.Item label="Kysucké Nové Mesto" value="Kysucké Nové Mesto" />
+              <Picker.Item label="Levice" value="Levice" />
+              <Picker.Item label="Levoča" value="Levoča" />
+              <Picker.Item label="Liptovský Mikuláš" value="Liptovský Mikuláš" />
+              <Picker.Item label="Lučenec" value="Lučenec" />
+              <Picker.Item label="Malacky" value="Malacky" />
+              <Picker.Item label="Martin" value="Martin" />
+              <Picker.Item label="Medzilaborce" value="jMedzilaborce" />
+              <Picker.Item label="Michalovce" value="Michalovce" />
+              <Picker.Item label="Myjava" value="Myjava" />
+              <Picker.Item label="Námestovo" value="Námestovo" />
+              <Picker.Item label="Nitra" value="Nitra" />
+              <Picker.Item label="Nové Mesto nad Váhom" value="Nové Mesto nad Váhom" />
+              <Picker.Item label="Nové Zámky" value="Nové Zámky" />
+              <Picker.Item label="Partizánske" value="Partizánske" />
+              <Picker.Item label="Pezinok" value="Pezinok" />
+              <Picker.Item label="Piešťany" value="Piešťany" />
+              <Picker.Item label="Poltár" value="Poltár" />
+              <Picker.Item label="Poprad" value="Poprad" />
+              <Picker.Item label="Považská Bystrica" value="Považská Bystrica" />
+              <Picker.Item label="Prešov" value="Prešov" />
+              <Picker.Item label="Prievidza" value="Prievidza" />
+              <Picker.Item label="Púchov" value="Púchov" />
+              <Picker.Item label="Revúca" value="Revúca" />
+              <Picker.Item label="Rimavská Sobota" value="Rimavská Sobota" />
+              <Picker.Item label="Rožňava" value="Rožňava" />
+              <Picker.Item label="Ružomberok" value="Ružomberok" />
+              <Picker.Item label="Sabinov" value="Sabinov" />
+              <Picker.Item label="Senec" value="Senec" />
+              <Picker.Item label="Senica" value="Senica" />
+              <Picker.Item label="Skalica" value="Skalica" />
+              <Picker.Item label="Snina" value="Snina" />
+              <Picker.Item label="Sobrance" value="Sobrance" />
+              <Picker.Item label="Spišská Nová Ves" value="Spišská Nová Ves" />
+              <Picker.Item label="Stará Ľubovňa" value="Stará Ľubovňa" />
+              <Picker.Item label="Stropkov" value="Stropkov" />
+              <Picker.Item label="Svidník" value="Svidník" />
+              <Picker.Item label="Šaľa" value="Šaľa" />
+              <Picker.Item label="Topoľčany" value="Topoľčany" />
+              <Picker.Item label="Trebišov" value="Trebišov" />
+              <Picker.Item label="Trenčín" value="Trenčín" />
+              <Picker.Item label="Trnava" value="Trnava" />
+              <Picker.Item label="Turčianske Teplice" value="Turčianske Teplice" />
+              <Picker.Item label="Tvrdošín" value="Tvrdošín" />
+              <Picker.Item label="Veľký Krtíš" value="Veľký Krtíš" />
+              <Picker.Item label="Vranov nad Topľou" value="Vranov nad Topľou" />
+              <Picker.Item label="Zlaté Moravce" value="Zlaté Moravce" />
+              <Picker.Item label="Zvolen" value="Zvolen" />
+              <Picker.Item label="Žarnovica" value="Žarnovica" />
+              <Picker.Item label="Žiar nad Hronom" value="Žiar nad Hronom" />
+              <Picker.Item label="Žilina" value="Žilina" />
+            </Picker>
+          </View>
         </View>
       )}
       <ItemsStyled>
         <LinkStyled to="/Login">Chceš sa prihlásiť? Klikni sem</LinkStyled>
-        <Button mode="contained" onPress={register}>
+        <Button mode="contained"  onPress={()=>register(firstname, lastname,name, email, password,city,street,zipcode,phone,district)}>
           Registrovať sa
         </Button>
         <LinkStyled to="/NotLoggedApp/Domov">
@@ -103,6 +243,7 @@ const Register = ({navigation}) => {
   );
 };
 export default Register;
+
 
 const InputStyled = styled(TextInput)`
   margin-vertical: 2%;
@@ -117,3 +258,16 @@ const LinkStyled = styled(Link)`
   text-decoration-style: solid;
   text-decoration-color: #000;
 `;
+
+const styles = StyleSheet.create({
+  picker: {
+    color: '#000',
+    width: 300,
+    height: 50,
+  },
+  pickerview: {
+    borderColor: '#A9A9A9',
+    borderWidth: 1,
+    marginTop: 15,
+  }
+}) 
